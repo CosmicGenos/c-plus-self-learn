dcefdfbcf06b5e893230baad3ee15e2a826673a7
CosmicGenos <nimesha12417@usci.ruh.ac.lk>
2026-06-30 22:57:08 +0530

    new

--- c-plus-self-learn/c-plus-self-learn/src/main.cpp
+++ c-plus-self-learn/c-plus-self-learn/src/main.cpp
@@ -6,3 +6,25 @@ int main()
 	return 0;
 	
 }
+
+//#include <cstdio>
+//using namespace std;
+//
+//int main()
+//{
+//	puts("Click on Button");
+//	puts("Create a new player");
+//	puts("Add life to player");
+//	return 0;
+//}
+
+
+char is_char(char c)
+{
+	return c; 
+}
+
+void ask_user_input_color(char* color)
+{
+	std::cout << "Hey my favorite color also" << color << "\n";
+}

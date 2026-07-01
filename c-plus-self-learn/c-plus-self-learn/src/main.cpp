71e9ea5847f7e4ecdf3ec148515eddb135d7e5e8
CosmicGenos <nimesha12417@usci.ruh.ac.lk>
2026-07-02 00:08:55 +0530

    colour ex - 4

--- c-plus-self-learn/c-plus-self-learn/src/main.cpp
+++ c-plus-self-learn/c-plus-self-learn/src/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 void ask_user_input_color(char* color);
+void switch_1(int& p, int& q);
 
 // namespace not that complictated , its like using function inside a class,
 // but in this case we are using function inside a namespace, so we can use the function without using namespace name
@@ -18,6 +19,20 @@ int main()
 	cout << "Enter your Last Name: ";
 	cin.getline(str2, 100);
 	cout << "Hello, " << str1 << " " << str2 << endl;
+
+	int** p;
+	int rows = 3;
+	int* cols = new int[rows];
+	p = &cols;
+
+	cout << p << endl;
+
+	int a = 10;
+	int b = 20;
+
+	cout << "Before switch: a = " << a << ", b = " << b << endl;
+	switch_1(a, b);
+	cout << "After switch: a = " << a << ", b = " << b << endl;
 
 	return 0;
 	
@@ -43,4 +58,11 @@ char is_char(char c)
 void ask_user_input_color(char* color)
 {
 	std::cout << "Hey my favorite color also " << color << "\n";
+}
+
+void switch_1 (int &p, int &q )
+{
+	int temp = p;
+	p = q;
+	q = temp;
 }

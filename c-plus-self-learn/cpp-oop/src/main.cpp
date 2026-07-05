c5af730cfd62426d8099cd101bb1c3d613936c0b
CosmicGenos <nimesha12417@usci.ruh.ac.lk>
2026-07-05 20:41:44 +0530

    oop push - 1

--- c-plus-self-learn/cpp-oop/src/main.cpp
+++ c-plus-self-learn/cpp-oop/src/main.cpp
@@ -5,19 +5,52 @@ using namespace std;
 
 
 class Point {
-public:
 	int x;
 	int y;
+
+	public:
+		Point()
+		{
+			x = 0;
+			y = 0;
+		
+			cout << "Point created at (" << x << ", " << y << ")" << endl;
+		}
+
+		Point(int x, int y)
+		{
+			this->x = x;
+			this->y = y;
+			cout << "Point created at (" << x << ", " << y << ")" << endl;
 	
-	void display() {
-		cout << "Point(" << x << ", " << y << ")" << endl;
-	}
+		}
+		void display() {
+			cout << "Point(" << x << ", " << y << ")" << endl;
+		}
 
-	double distanceTo(const Point &other)
-	{
-		return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
-	}
+		double distanceTo(const Point &other)
+		{
+			return sqrt(pow(x - other.getX(),2) + pow(y - other.getY(), 2));
+		}
 
+		int getX() const {
+			return this->x;
+		}
+
+		int getY() const {
+			return this->y;
+		}
+
+		void setY(int y)
+		{
+			this->y = y;
+		}
+
+		void setX(int x)
+		{
+			this->x = x;
+		}
+	
 };
 
 int main()
@@ -33,15 +66,22 @@ int main()
 	}
 	
 	Point p1;
-	p1.x = 10;
-	p1.y = 20;
+	p1.setX(10);
+	p1.setY(20);
 
 	p1.display();
 
 	for (auto& point : points) {
 		double distance = point.distanceTo(p1);
-		cout << "Distance from Point(" << point.x << ", " << point.y << ") to Point(" << p1.x << ", " << p1.y << ") is: " << distance << endl;
+		cout << "Distance from Point(" << point.getX() <<", " << point.getY()<<") to Point(" << p1.getY() << ", " << p1.getY() << ") is: " << distance << endl;
 	}
+
+	for (auto& point : points) {
+		point.setX(point.getX() + 1);
+		point.setY(point.getY() + 1);
+	}
+
+
 
 	return 0;
 }
